e4071381100baab6b081e329f1101b8ac3d95340
sandip2006 <138397632+sandip2006@users.noreply.github.com>
2023-07-09 14:37:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "24579842"))
+API_HASH = environ.get("API_HASH", "ec6105bf1a02c98f837300546dc341d1")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6318544168:AAG_wwukpV_yhqGGMCL3VtKhPZom3igCtaA")
+ADMIN = int(environ.get("ADMIN", "1991522624"))          
+CAPTION = environ.get("CAPTION", "<b>ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ   -   <a href=https://t.me/The_Happy_Hour_Hindi>ᴛʜᴇ ʜᴀᴘᴘʏ ʜᴏᴜʀ</a></b>
+<b>ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ - <a href=https://t.me/Happy_Hour_Friends>ʜᴀᴘᴘʏ ʜᴏᴜʀ ɢʀᴏᴜᴘ</a></b>")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
