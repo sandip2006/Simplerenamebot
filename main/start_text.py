c451c87c3cc73115b4a141679fb9c054c6911ab3
sandip2006 <138397632+sandip2006@users.noreply.github.com>
2023-07-09 10:54:18 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -5,11 +5,11 @@ from config import ADMIN
 
 @Client.on_message(filters.command("start") & filters.private)                             
 async def start_cmd(bot, msg):
-    txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
+    txt="This is personal use bot 🙏. Do you want your own bot? 👇 join channel and use me💝"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 SOURCE CODE", url="https://t.me/The_Happy_Hour_Hindi")
+        InlineKeyboardButton("Backup Channel", url="https://t.me/The_Happy_Hour_Hindi")
         ],[
-        InlineKeyboardButton("🖥️ How To Deploy", url="https://t.me/The_Happy_Hour_Hindi")
+        InlineKeyboardButton("Join Channel", url="https://t.me/The_Happy_Hour_Hindi")
     ]])
     if msg.from_user.id != ADMIN:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
@@ -18,9 +18,9 @@ async def start_cmd(bot, msg):
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/MrMKN>MrMKN</a></b>"                                     
+    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://t.me/The_Happy_Hour_Hindi</a></b>"                                     
     button= [[
-        InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/The_Happy_Hour_Hindi")
+        InlineKeyboardButton("💝 Backup Channel 💝", url="https://t.me/The_Happy_Hour_Hindi")
         ],[
         InlineKeyboardButton("ℹ️ Help", callback_data="help"),
         InlineKeyboardButton("📡 About", callback_data="about") 
@@ -47,9 +47,9 @@ async def help(bot, msg):
 @Client.on_callback_query(filters.regex("about"))
 async def about(bot, msg):
     me=await bot.get_me()
-    Master=f"<a href=https://t.me/Mo_Tech_YT>MoTech</a> & <a href=https://t.me/venombotupdates>MhdRzn</a>"  
-    Source="<a href=https://github.com/MrMKN/Simple-Rename-Bot>Click Here</a>"
-    txt=f"<b>Bot Name: {me.mention}\nDeveloper: <a href=https://github.com/MrMKN>MrMKN</a>\nBot Updates: <a href=https://t.me/mkn_bots_updates>Mᴋɴ Bᴏᴛᴢ™</a>\nMy Master's: {Master}\nSource Code: {Source}</b>"                 
+    Master=f"<a href=https://t.me/The_Happy_Hour_Fans>Movie Request group</a> & <a href=https://t.me/venombotupdates>MhdRzn</a>"  
+    Source="<a href=https://t.me/The_Happy_Hour_Hindi>join channel</a>"
+    txt=f"<b>Bot Name: {me.mention}\nDeveloper: <a href=https://t.me/Mister_Ash</a>\nBot Updates: <a href=https://t.me/The_Happy_Hour_Hindi>Mᴋɴ Bᴏᴛᴢ™</a>\nMy Master's: {Master}\nSource Code: {Source}</b>"                 
     button= [[        
         InlineKeyboardButton("🚫 Close", callback_data="del"),
         InlineKeyboardButton("⬅️ Back", callback_data="start") 
